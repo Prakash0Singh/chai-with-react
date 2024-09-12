08d8daf71ac7f9fbb03ae853be0a9b954811f743
Prakash Singh Basnal <72181041+Prakash0Singh@users.noreply.github.com>
2024-09-12 07:18:33 +0000

    context-functionality

--- toDoList-Local/src/App.jsx
+++ toDoList-Local/src/App.jsx
@@ -6,8 +6,21 @@ import { ToDoProvider } from './contexts';
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const addTodo=(todo)=>{
 
+  const addTodo=(todo)=>{
+    setTodos((prev)=>[{id:Date.now(),...todo},...prev]);
+  }
+
+  const updateTodo=(id,todo)=>{
+    setTodos((prev)=>prev.map((prevTodo)=>{ prevTodo.id === id ? todo :prevTodo }));
+  }
+
+  const deleteTodo =(id)=>{
+    setTodos((prev)=>prev.filter((prevTodo)=> prevTodo.id !== id ));
+  }
+
+  const toggleComplete=(id)=>{
+    setTodos((prev)=>prev.map( (prevTodo)=> prevTodo.id === id ? {...prevTodo,completed:!prevTodo.completed} :prevTodo ))
   }
 
   return (
